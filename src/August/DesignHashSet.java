93d6d110524efdd518946a85a8f4d5045cc5b723
Ananya Sen <ananya.sen@infoedge.com>
2020-08-03 18:55:21 +0530

    [master] Committing code for August Leet Code challenge 3

--- src/August/DesignHashSet.java
+++ src/August/DesignHashSet.java
@@ -4,8 +4,10 @@ public class DesignHashSet {
 
   public static void main(String[] args) {
     MyHashSet obj = new MyHashSet();
-    int key = 1;
+    int key = 100;
+    System.out.println(obj.contains(key));
     obj.add(key);
+    System.out.println(obj.contains(key));
     obj.remove(key);
     System.out.println(obj.contains(key));
   }
@@ -15,24 +17,88 @@ public class DesignHashSet {
 
 class MyHashSet {
 
-  int[] mySet;
+  class Node {
+    int data;
+    Node next;
 
-  public MyHashSet() {
-    if (mySet != null) {
-      mySet[]
+    Node(int val) {
+      this.data = val;
     }
   }
 
-  public void add(int key) {
+  int size;
+  Node[] arrayNode;
+
+  public MyHashSet() {
+    size = 100;
+    arrayNode = new Node[100];
+  }
+
+  public void add(int newData) {
+    int index = (newData % this.size);
+    // When the index of this key does not exists already
+    if (this.arrayNode[index] == null) {
+      this.arrayNode[index] = new Node(newData);
+      return;
+    }
+
+    // When the index exits and Data also exits
+    Node currNode = this.arrayNode[index];
+    while (currNode != null && currNode.data != newData) {
+      currNode = currNode.next;
+    }
+    if (null != currNode) {
+      return;
+    }
+
+    // When the index exits and Data does not exits i.e. Data is new
+
+    Node newHead = new Node(newData);
+    newHead.next = this.arrayNode[index];
+    this.arrayNode[index] = newHead;
 
   }
 
-  public void remove(int key) {
+  public void remove(int newData) {
+    int index = (newData % this.size);
+    Node currNode = this.arrayNode[index];
+    // Index does not exists
+    if (null == currNode) {
+      return;
+    } else {
+      // Index Exists - Check if it is the head node
+      if (currNode.data == newData) {
+        this.arrayNode[index] = this.arrayNode[index].next;
+        return;
+      } else {
+        // Index Exists - Check if it is NOT the head node
+        while (currNode.next != null && currNode.next.data != newData) {
+          currNode = currNode.next;
+        }
 
+        // When you reach last node and do not find the data
+        if (currNode.next == null) {
+          return;
+        }
+        currNode.next = currNode.next.next;
+      }
+    }
   }
 
   /** Returns true if this set contains the specified element */
-  public boolean contains(int key) {
+  public boolean contains(int newData) {
+    int index = (newData % this.size);
+    Node currNode = this.arrayNode[index];
+    // When Index does not exists
+    if (null == currNode) {
+      return false;
+    } else {
+      // When Index exits - Traverse the LL to check if data exists
+      while (currNode != null && currNode.data != newData) {
+        currNode = currNode.next;
+      }
 
+      return (null != currNode);
+    }
   }
 }
