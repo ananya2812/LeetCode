5047f05d33cc07c82b30a658bcb3dd097b2b0647
Ananya Sen <ananya.sen@infoedge.com>
2020-08-03 19:43:12 +0530

    [master] April Leet code challenge

--- src/April/SingleNumber.java
+++ src/April/SingleNumber.java
@@ -1,5 +1,14 @@
 package April;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single
+ * one.
+ * 
+ * Input: [2,2,1] Output: 1
+ */
 public class SingleNumber {
 
   public static void main(String[] args) {
@@ -7,6 +16,25 @@ public class SingleNumber {
     System.out.println(singleNumber(input));
   }
 
+  private static int singleNumber2(int[] nums) {
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i : nums) {
+      map.put(i, map.getOrDefault(i, 0) + 1);
+    }
+
+    for (int i : nums) {
+      if (map.get(i) == 1) {
+        return i;
+      }
+    }
+    return 0;
+  }
+
+
+  /*
+   * XOR(a,0) = a | XOR(a,a) = 0
+   */
   private static int singleNumber(int[] nums) {
     int a = 0;
     for (int i = 0; i < nums.length; i++) {
@@ -14,4 +42,5 @@ public class SingleNumber {
     }
     return a;
   }
+
 }
